7eeb32692ec2d8897d21e6c375d4cd69e406bf68
Vladislav Russo <vladtirma@gmail.com>
2022-02-21 06:24:33 +0300

    Version 1.1

--- Assets/Script/ClickRandomColor.cs
+++ Assets/Script/ClickRandomColor.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class ClickRandomColor : MonoBehaviour
 {
     [SerializeField] private GameObject clickNumber;
-    public void OnMouseDown()
+    public void FixedUpdate()
     {
-        GameObject gameObject = Instantiate(clickNumber, transform.position, Quaternion.identity);
-        Destroy(gameObject, 1f);
-        GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        if (Input.touchCount > 0)
+        {
+            GameObject gameObject = Instantiate(clickNumber, transform.position, Quaternion.identity);
+            Destroy(gameObject, 0.2f);
+            GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        }
     }
     
 }
